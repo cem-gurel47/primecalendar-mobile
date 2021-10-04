d17d4078fd00e7a2d7ba74292994aebcf9cd194a
Cem Gurel <gurel.cem374@hotmail.com>
2021-10-03 21:54:37 -0400

    initial commit with colors

--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -3,8 +3,16 @@ const constants = {
   secondary: '#4860B3',
   highPriority: '#FE6E6F',
   mediumPriority: '#F2994A',
+  gradient: '#3D70F8',
   gray: {
     1: '#F2F2F2',
+    2: '#E0E0E0',
+    3: '#BDBDBD',
+    4: '#828282',
+    5: '#4F4F4F',
+    6: '#333333',
+    7: '#1D1D1D',
+    8: '#171717',
   },
 };
 
