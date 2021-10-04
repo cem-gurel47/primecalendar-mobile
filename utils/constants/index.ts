091e359f310d4e00901b3dc0a30ff688ccbf0fe4
Cem Gurel <cgure085@uottawa.ca>
2021-10-03 20:55:34 -0400

    initial commit with configs

---
+++ utils/constants/index.ts
@@ -0,0 +1,11 @@
+const constants = {
+  primary: '#B35648',
+  secondary: '#4860B3',
+  highPriority: '#FE6E6F',
+  mediumPriority: '#F2994A',
+  gray: {
+    1: '#F2F2F2',
+  },
+};
+
+export default constants;
